8b2c80e80520dadab052c01ffc5262e1070d5116
Silviu Stoian <silviu.stoian96@gmail.com>
2016-08-04 21:05:12 +0300

    update

--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -1,1 +1,33 @@
 #write your code here
+def echo(something)
+   something
+
+end
+
+def shout(some)
+  some.upcase #upcase will capitalize all
+
+end
+
+def repeat(some,n_o_t=2)  #initialize number of times with 2, so 1st ex good
+  word= some + " "
+  return (word * n_o_t).strip
+
+end
+
+
+
+def start_of_word(word, num)
+  return word[0..num-1]
+end
+
+def first_word(word)
+  word.split.first
+
+end
+
+def titleize(string)
+  arr = string.split(' ').each { |word| word.capitalize! unless (word == "and" || word == "over" || word == "the") }
+  arr[0].capitalize!
+  return arr.join(' ')
+end
