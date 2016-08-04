2ebf33916f9cf3c134decb540394042ad515060e
Silviu Stoian <silviu.stoian96@gmail.com>
2016-08-04 17:41:50 +0300

    update

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,38 @@
 #write your code here
+
+def add(x,y)
+  x+y
+end
+
+def subtract(x,y)
+  x-y
+end
+
+def sum(array)
+  total=0
+  array.each{|n| total+=n}
+  total
+end
+
+def multiply(*numbers)
+  result=1
+  numbers.each {|n| result*=n}
+  result
+end
+
+def power(x,y)
+  x**y
+
+end
+
+def factorial(n)
+  if n==0
+    return 0
+  else
+  (1..n).inject(:*) || 1
+
+end
+
+end
+
+puts factorial(10)
