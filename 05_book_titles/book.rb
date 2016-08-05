b32849b2c0ff10e74d8e2103634e5ccb9b4e083c
Silviu Stoian <silviu.stoian96@gmail.com>
2016-08-05 22:39:47 +0300

    update

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,21 @@
-class Book
-# write your code here
+def titleize(phrase)
+
+  exception=["and" , "or" , "but" , "yet" , "for" , "nor" ,
+     "so","as" , "at" , "by" , "in" , "of" , "to","the" , "a" , "an"]
+  arr=phrase.split(" ").each{|word| word.capitalize! unless exception.include?(word) }
+  arr[0].capitalize!
+  return arr.join(' ')
 end
+class Book
+  attr_accessor :title
+
+  def title=(new_title)
+   puts @title=titleize(new_title)
+  end
+
+
+end
+
+book=Book.new
+
+book.title=("inferno or the beggininnggnagg of the wordssr")
